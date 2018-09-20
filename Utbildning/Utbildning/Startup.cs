8a01cbce29340d6f77bf429334fb7329daf7d31f
olle.sundbergg <olle.sundbergg@gmail.com>
2018-09-20 11:21:02 +0200

    Edit startup.cs to automatically create roles on startup. Add simple pages that will eventually be admin related.

--- Utbildning/Utbildning/Startup.cs
+++ Utbildning/Utbildning/Startup.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using Utbildning.Models;
 
 [assembly: OwinStartupAttribute(typeof(Utbildning.Startup))]
 namespace Utbildning
@@ -9,6 +12,48 @@ namespace Utbildning
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            CreateRolesAndDefaultUsers();
+        }
+
+        private void CreateRolesAndDefaultUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var RoleMng = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserMng = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            if (!RoleMng.RoleExists("Admin"))
+            {
+                var AdminRole = new IdentityRole() { Name = "Admin" };
+                RoleMng.Create(AdminRole);
+
+                var AdminUser = new ApplicationUser() { UserName = "admin@m.m" };
+                string Pw = "Mypass123!";
+
+                var NewUser = UserMng.Create(AdminUser, Pw);
+
+                if (NewUser.Succeeded)
+                {
+                    var result = UserMng.AddToRole(AdminUser.Id, "Admin");
+                }
+            }
+
+            if (!RoleMng.RoleExists("Kursledare"))
+            {
+                var KLRole = new IdentityRole() { Name = "Kursledare" };
+                RoleMng.Create(KLRole);
+
+                //REMOVE FOLLOWING LATER WHEN OBSOLETE. Only used to have a default KL user.
+                var KLUser = new ApplicationUser() { UserName = "kl@m.m" };
+                string Pw = "Mypass123!";
+
+                var NewUser = UserMng.Create(KLUser, Pw);
+
+                if (NewUser.Succeeded)
+                {
+                    var result = UserMng.AddToRole(KLUser.Id, "Kursledare");
+                }
+            }
         }
     }
 }
