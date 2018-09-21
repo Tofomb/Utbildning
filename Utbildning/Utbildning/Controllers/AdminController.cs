e7c98a634b7a51061bc8a5f0ddc0b9fa6793a4aa
Tofomb <tobias.c.forsberg@gmail.com>
2018-09-21 11:28:59 +0200

    added kursvy and kurstilfälle

--- Utbildning/Utbildning/Controllers/AdminController.cs
+++ Utbildning/Utbildning/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNet.Identity.Owin;
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.Owin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -148,6 +149,23 @@ namespace Utbildning.Controllers
             ViewBag.Name = new string(NameChar);
 
             return View();
+        }
+        [Authorize(Roles = "Kursledare")]
+        public ActionResult Kursvy()
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            return View(db.Courses.ToList().Where(m=>m.Email == User.Identity.Name));
+            
+        }
+
+
+        // GET: CourseOccasions
+        [Authorize(Roles = "Kursledare")]
+        public ActionResult Kurstillfälle(int? id)
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            var courseOccasions = db.CourseOccasions.Include(c =>c.Course);
+            return View(courseOccasions.ToList().Where(m => m.Course.Email == User.Identity.Name && m.Course.Id == id));
         }
     }
 }
