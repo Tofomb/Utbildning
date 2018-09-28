8f6c9df7958c250c21167b5812e501a5bd5e27fa
olle.sundbergg <olle.sundbergg@gmail.com>
2018-09-28 11:27:53 +0200

    Add profile configuration

---
+++ Utbildning/Utbildning/Areas/Kursledare/Controllers/ProfilController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Utbildning.Classes;
+using Utbildning.Models;
+
+namespace Utbildning.Areas.Kursledare.Controllers
+{
+    public class ProfilController : Controller
+    {
+        // GET: Kursledare/Profil
+        [NoCache]
+        [Authorize(Roles = "Kursledare")]
+        public ActionResult Index()
+        {
+            string Id = User.Identity.GetUserId();
+            ViewBag.FullName = User.GetFullName();
+            using (var db = new ApplicationDbContext())
+            {
+                ApplicationUser user = db.Users.Find(Id);                
+                return View(user);
+            }
+        }
+
+        [Authorize(Roles = "Kursledare")]
+        public ActionResult Namn()
+        {
+            ViewBag.FullName = User.GetFullName();
+            return View();
+        }
+
+        [Authorize(Roles = "Kursledare")]
+        [HttpPost]
+        public ActionResult Namn(ChangeFullNameViewModel model)
+        {
+            User.SetFullName(model.FullName);
+            return Redirect("~/Kursledare/Profil");
+        }
+
+        [Authorize(Roles = "Kursledare")]
+        public ActionResult Bild()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Kursledare")]
+        [HttpPost]
+        public ActionResult Bild(HttpPostedFileBase file)
+        {
+            if (file != null)
+            {
+                string imageName = User.Identity.GetUserId() + Path.GetExtension(file.FileName);
+                string path = Path.Combine(Server.MapPath("~/images/profile"), imageName);                
+
+                file.SaveAs(path);
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    file.InputStream.CopyTo(ms);
+                    byte[] array = ms.GetBuffer();
+                }
+            }
+
+            return Redirect("~/Kursledare/Profil");
+        }
+    }
+
+    public class NoCache : ActionFilterAttribute
+    {
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            filterContext.HttpContext.Response.Cache.SetExpires(DateTime.UtcNow.AddDays(-1));
+            filterContext.HttpContext.Response.Cache.SetValidUntilExpires(false);
+            filterContext.HttpContext.Response.Cache.SetRevalidation(HttpCacheRevalidation.AllCaches);
+            filterContext.HttpContext.Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            filterContext.HttpContext.Response.Cache.SetNoStore();
+
+            base.OnResultExecuting(filterContext);
+        }
+    }
+}
