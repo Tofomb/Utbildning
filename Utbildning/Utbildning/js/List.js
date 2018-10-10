1bf16e3c43bcdca5262a43284a7d7e223a45faac
olle.sundbergg <olle.sundbergg@gmail.com>
2018-10-10 11:09:52 +0200

    Add stuff

---
+++ Utbildning/Utbildning/js/List.js
@@ -0,0 +1,21 @@
+﻿function search() {
+    var input, filter, list, li, title, i;
+    input = document.getElementById("search");
+    filter = input.value.toUpperCase();
+    list = document.getElementById("listbox");
+    li = list.getElementsByClassName("container-list");
+
+    console.log(list.childElementCount);
+
+    for (i = 0; i < li.length; i++) {
+        console.log("i = " + i);
+        title = li[i].getElementsByClassName("container-info")[0].getElementsByClassName("text-title")[0];
+        if (title.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            console.log("title = " + title);
+            li[i].style.display = "";
+        } else {
+            console.log("title = " + title);
+            li[i].style.display = "none";
+        }
+    }
+}
