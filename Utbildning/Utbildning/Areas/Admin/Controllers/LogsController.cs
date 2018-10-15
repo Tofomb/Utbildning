f204a5430d70143dbcaa34dd0a4be6fad6a08ff3
Tofomb <tobias.c.forsberg@gmail.com>
2018-10-15 15:43:39 +0200

    Added Logview for admin

---
+++ Utbildning/Utbildning/Areas/Admin/Controllers/LogsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Utbildning.Models;
+
+namespace Utbildning.Areas.Admin.Controllers
+{
+    public class LogsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Admin/Logs
+        [Authorize(Roles = "Admin")]
+        public ActionResult Index()
+        {
+            return View(db.Logs.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
