937c41fa8523cee1cdcd5f36b1fbfbfd4151b2d4
Tofomb <tobias.c.forsberg@gmail.com>
2018-10-19 13:11:13 +0200

    translated some views to swedish

--- Utbildning/Utbildning/Controllers/KurserController.cs
+++ Utbildning/Utbildning/Controllers/KurserController.cs
@@ -39,13 +39,13 @@ namespace Utbildning.Controllers
             List<string> CTList = new List<string>();
             using (ApplicationDbContext db = new ApplicationDbContext())
             {
-                foreach(var a in x)
+                foreach (var a in x)
                 {
                     CTList.AddRange(from b in db.CourseTags.Where(b => b.CourseId == a.Id) select b.Text);
                 }
             }
             var Tags = CTList.GroupBy(a => a).OrderByDescending(grp => grp.Count()).Select(grp => grp.Key).ToList();
-            
+
             ViewBag.Tags = Tags;
 
             return View(CLIs.ToList());
@@ -109,7 +109,7 @@ namespace Utbildning.Controllers
                             db.SaveChanges();
 
                             // Tester
-                            string MailText = DBHandler.GetCourse(DBHandler.GetCourseOccasion(booking.CourseOccasionId)).Name + " " + DBHandler.GetCourseOccasion(booking.CourseOccasionId).StartDate.Format() + "\n Tack för din bokning, " + booking.Firstname + " " + booking.Lastname + "\n Platser:" + booking.Bookings + "\n Om du har några frågor, hör av dig till kursansvarig: " + DBHandler.GetCourse(DBHandler.GetCourseOccasion(booking.CourseOccasionId)).Email + $"<br/>Avbokning: <a href='" + URLHandler.GetBaseUrl(Request.Url) + "/Kurser/Avboka?email={URLHandler.GenAUId(booking.Email)}' Avboka";
+                            string MailText = DBHandler.GetCourse(DBHandler.GetCourseOccasion(booking.CourseOccasionId)).Name + " " + DBHandler.GetCourseOccasion(booking.CourseOccasionId).StartDate.Format() + "\n Tack för din bokning, " + booking.Firstname + " " + booking.Lastname + "\n Platser:" + booking.Bookings + "\n Om du har några frågor, hör av dig till kursansvarig: " + DBHandler.GetCourse(DBHandler.GetCourseOccasion(booking.CourseOccasionId)).Email + $"<br/>Avbokning: <a href='" + URLHandler.GetBaseUrl(Request.Url) + $"/Kurser/Avboka?email={URLHandler.GenAUId(booking.Email + booking.Id)}' Avboka";
                             string MailTextKL = "Ny bokning \n" + DBHandler.GetCourse(DBHandler.GetCourseOccasion(booking.CourseOccasionId)).Name + " " + DBHandler.GetCourseOccasion(booking.CourseOccasionId).StartDate.Format() + "\n" + booking.Firstname + " " + booking.Lastname + "\n Platser:" + booking.Bookings;
 
                             // MailHandler.SendTester("", booking.Email, "Bokningsbekräftelse",  MailText, "");
@@ -196,10 +196,11 @@ namespace Utbildning.Controllers
                 UserData += "\n \n";
 
             }
-         
-            
-            string MailText = "Du efterfrågade en utskrit om all data som Castra Utbildning har sparat kopplat till din mail, om du önskar att ta bort information från vår databas, tryck på länken nedanför. Var medveten om att detta även betyder att bokningen kopplad till din email kommer att avbokas.<br/><a href='" + URLHandler.GetBaseUrl(Request.Url) + $"/Kurser/Avboka?email={URLHandler.GenAUId(UserEmail)}'>Klicka här</a>";
-             string SecretEmail = UserEmail;
+
+
+            //   string MailText = "Du efterfrågade en utskrit om all data som Castra Utbildning har sparat kopplat till din mail, om du önskar att ta bort information från vår databas, tryck på länken nedanför. Var medveten om att detta även betyder att bokningen kopplad till din email kommer att avbokas.<br/><a href='" + URLHandler.GetBaseUrl(Request.Url) + $"/Kurser/Avboka?email={URLHandler.GenAUId(UserEmail)}'>Klicka här</a>";
+            string MailText = "Du efterfrågade en utskrit om all data som Castra Utbildning har sparat kopplat till din mail, om du önskar att ta bort information från vår databas, ta kontakt med kursledaren. Om hen inte svarar inom rimlig tid så får du gärna ta kontakt med hemsidans administratör.";
+            string SecretEmail = UserEmail;
 
 
             // MailHandler.Send(UserEmail, "Användardata Castra-utbildning", UserData + MailText);
@@ -227,18 +228,23 @@ namespace Utbildning.Controllers
         public ActionResult Avboka(string user, string url)
         {
             //TODO maila kursledaren
-            List<Booking> bookings = db.Bookings.ToList().Where(x => URLHandler.GenAUId(x.Email) == user).ToList();
-            foreach (Booking b in bookings)
-                if (DBHandler.GetCourseOccasion(b).StartDate > DateTime.Now)
-                    db.Bookings.Remove(b);
-            db.SaveChanges();
+            Booking booking = db.Bookings.ToList().Where(x => URLHandler.GenAUId(x.Email + x.Id) == user).ToList().First();
+
+            if (DBHandler.GetCourseOccasion(booking).StartDate > DateTime.Now)
+            {
+                db.SaveChanges();
+                db.Bookings.Remove(booking);
+            }
+            else
+                return Redirect("~Kurser/Avboka?w=cas");
 
             return View();
         }
 
         public ActionResult Om()
         {
-            using (ApplicationDbContext db = new ApplicationDbContext()) {
+            using (ApplicationDbContext db = new ApplicationDbContext())
+            {
                 ViewBag.Title = db.SiteConfigurations.Where(x => x.Property == "AboutTitle").First().Value;
                 ViewBag.Bold = db.SiteConfigurations.Where(x => x.Property == "AboutBold").First().Value;
                 ViewBag.Text = db.SiteConfigurations.Where(x => x.Property == "AboutText").First().Value;
